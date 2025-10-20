94065d25c88d5a613bd9677635a0d79a92d999c9
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-20 09:27:23 +0200

    feat: bubbleSort

--- Fibonnaci.cpp
+++ Fibonnaci.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n){
-    if(n<2){
+int fibonacci(int n) {
+    if (n < 2) {
         return n;
     }
-    return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main () {
-    cout <<
+int main() {
+    int n;
+    cout << "Enter the position (n): ";
+    cin >> n;
+
+    cout << "Fibonacci(" << n << ") = " << fibonacci(n) << endl;
+
+    return 0;
 }
