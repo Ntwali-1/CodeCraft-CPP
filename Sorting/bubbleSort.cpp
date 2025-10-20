f96063280efd8aeae6255c1ebb6d126e3ef06f39
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-20 16:39:40 +0200

    update: bubbleSort

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -2,17 +2,25 @@
 using namespace std;
 
 int main () {
-    int arr[5] = {4,6,3,5,2};
+    int arr[6] = {10,20,30,40,50,60};
+    int n = sizeof(arr)/sizeof(int);
+    int count = 0;
+    bool check = 0;
 
-    for(int i=0;i<5-1;i++){
-        for(int j=0;j<5-i-1;j++){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            count++;
             if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                swap(arr[j],arr[j+1]);
             }
         }
+        if(check == 0){
+            break;
+        }
+        cout << check << endl;
     }
+    cout << count << endl;
+
 
     for(int i=0;i<5;i++){
         cout << arr[i] << endl;
